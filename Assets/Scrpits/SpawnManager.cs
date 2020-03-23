b78d2abb24aab3b77969bfba496e938223d7143b
Nicholas <nicholas.wilson4205@gmail.com>
2020-03-23 12:37:44 -0700

    Here is todays laesson
    For some reason my invoke repeating works but then says cant pull item or something. Need help to figue that one out

---
+++ Assets/Scrpits/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnLocation = new Vector3(15, 0, 0);
+    private float initalWait = 2;
+    private float periodRepeat = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawnobstacle", initalWait, periodRepeat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+    }
+}
