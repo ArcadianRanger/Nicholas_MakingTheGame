0ef5bb20ea1721a01fda35ca6116bef256651a30
Nicholas <nicholas.wilson4205@gmail.com>
2020-04-15 10:45:50 -0700

    Lesson 3D
    Lesson 3D

--- Assets/Scrpits/PlayerController.cs
+++ Assets/Scrpits/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     private Animator playerAnim;
+    public ParticleSystem obstacleExplosion;
+    public ParticleSystem dirtSplatt;
+    public AudioClip jumpSound;
+    public AudioClip explostionSound;
+    private AudioSource audioSource;
     public float forceMultiplier;
     public float gravityMultiplier;
     public bool onGround = true;
@@ -16,6 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
         Physics.gravity *= gravityMultiplier;
     }
 
@@ -26,7 +32,9 @@ public class PlayerController : MonoBehaviour
         {
             playerRigidbody.AddForce(Vector3.up * forceMultiplier, ForceMode.Impulse);
             onGround = false;
+            dirtSplatt.Stop();
             playerAnim.SetTrigger("Jump_trig");
+            audioSource.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -35,15 +43,19 @@ public class PlayerController : MonoBehaviour
 
         if(collision.gameObject.CompareTag("Obstacle"))
         {
+            dirtSplatt.Stop();
             gameOver = true;
+            obstacleExplosion.Play();
             Debug.Log("GameOver!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            audioSource.PlayOneShot(explostionSound, 1.0f);
         }
 
-        else if (collision.gameObject.CompareTag("Ground"))
+        else if (collision.gameObject.CompareTag("Ground") && !gameOver)
         {
             onGround = true;
+            dirtSplatt.Play();
         }
     }
 }
